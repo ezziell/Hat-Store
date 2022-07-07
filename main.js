4850f20c7c12d3d7c43ae4db56cbbc7494ae785a
eugenia <eugeniaalonsoc@gmail.com>
2022-07-07 15:19:47 -0300

    conectando mi api con frontend

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 
 const API = 'http://localhost:3000/api/v1/products';
-const titulo = document.getElementById("tituloUno");
-const imagen = document.getElementById("imagenUno");
+const imagen0 = document.getElementById("imagen0");
+const imagen1 = document.getElementById("imagen1");
+const imagen2 = document.getElementById("imagen2");
+const imagen3 = document.getElementById("imagen3");
+const imagen4 = document.getElementById("imagen4");
+const imagen5 = document.getElementById("imagen5");
+const imagen6 = document.getElementById("imagen6");
+const imagen7 = document.getElementById("imagen7");
+const imagen8 = document.getElementById("imagen8");
+const imagen9 = document.getElementById("imagen9");
+
+const todasImagenes = [imagen0, imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9];
 
 (async ()=> {
   const response = await fetch(API);
   const data = await response.json();
   console.log(data);
-  const nombre = data[1].name;
-  console.log(nombre);
-  titulo.innerHTML = nombre
-  imagen.src = data[1].image;
-  const img = document.createElement('img');
-  img.src = data[1].image;
+  todasImagenes[0].src = data[0].image;
+  todasImagenes[1].src = data[1].image;
+  todasImagenes[2].src = data[2].image;
+  todasImagenes[3].src = data[3].image;
+  todasImagenes[4].src = data[4].image;
+  todasImagenes[5].src = data[5].image;
+  todasImagenes[6].src = data[6].image;
+  todasImagenes[7].src = data[7].image;
 })();
-
-
-
